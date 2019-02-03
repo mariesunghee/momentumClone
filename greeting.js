a765ef005d7a3007ed7605bf9bc227ccbd6ee2b7
Mariesungheekim <mariesunghee@gmail.com>
2019-02-02 21:53:40 -0500

    Making a To Do List Part One

--- greeting.js
+++ greeting.js
@@ -1,29 +1,28 @@
-const form =document.querySelector(".js-form"),
-input=form.querySelector("input");
-const greeting=document.querySelector(".js-greeting");
+const form = document.querySelector(".js-form"),
+    input = form.querySelector("input"),
+    greeting = document.querySelector(".js-greetings");
 
 
-const USER_LS="currentUser";
-const SHOWING_CN="showing";
+const USER_LS = "currentUser",
+    SHOWING_CN = "showing";
 
-function saveName(text){
-    localStorage.setItem(USER_LS,text);
+function saveName(text) {
+    localStorage.setItem(USER_LS, text);
 }
 
-function handleSubmit(event){
+function handleSubmit(event) {
     event.preventDefault();
-    const currentValue=input.value;
+    const currentValue = input.value;
     paintGreeting(currentValue);
     saveName(currentValue);
 }
 
-
-function askForName(){
-        form.classList.add(SHOWING_CN);
-        form.addEventListener("submit",handleSubmit);
+function askForName() {
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
 }
 
-function paintGreeting(text){
+function paintGreeting(text) {
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
     greeting.innerText=`hello ${text}`;
@@ -34,7 +33,6 @@ function loadName(){
     if(currentUser===null){
         askForName()
     }else{
-
         paintGreeting(currentUser)
     }
 }
